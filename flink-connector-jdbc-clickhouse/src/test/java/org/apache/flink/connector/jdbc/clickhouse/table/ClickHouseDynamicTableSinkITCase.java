4d932cef2881fead88e116bb11b2976e41b99cc8
Biasqo <73300298+Biasqo@users.noreply.github.com>
2025-05-28 11:44:35 +0300

    Update ClickHouseDynamicTableSinkITCase.java
    test 2 without overriding

--- flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSinkITCase.java
+++ flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSinkITCase.java
@@ -21,76 +21,7 @@ package org.apache.flink.connector.jdbc.clickhouse.table;
 import org.apache.flink.connector.jdbc.clickhouse.ClickHouseTestBase;
 import org.apache.flink.connector.jdbc.clickhouse.database.dialect.ClickHouseDialect;
 import org.apache.flink.connector.jdbc.core.table.sink.JdbcDynamicTableSinkITCase;
-import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.types.Row;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.apache.flink.connector.jdbc.clickhouse.table.ClickHouseTableBuilder.clickHouseTableRow;
-import static org.apache.flink.connector.jdbc.clickhouse.table.ClickHouseTableBuilder.dbType;
-import static org.apache.flink.connector.jdbc.clickhouse.table.ClickHouseTableBuilder.field;
-import static org.apache.flink.connector.jdbc.clickhouse.table.ClickHouseTableBuilder.pkField;
 
 /** The Table Sink ITCase for {@link ClickHouseDialect}. */
 class ClickHouseDynamicTableSinkITCase extends JdbcDynamicTableSinkITCase
-        implements ClickHouseTestBase {
-
-    // ClickHouse does not support upsert statements
-    @Override
-    protected ClickHouseTableRow createUpsertOutputTable() {
-        return clickHouseTableRow(
-                "dynamSinkForUpsert",
-                pkField("cnt", DataTypes.BIGINT().notNull()),
-                field("lencnt", DataTypes.BIGINT().notNull()),
-                pkField("cTag", DataTypes.INT().notNull()),
-                field("ts", dbType("TIMESTAMP"), DataTypes.TIMESTAMP()));
-    }
-
-    @Override
-    protected ClickHouseTableRow createAppendOutputTable() {
-        return clickHouseTableRow(
-                "dynamicSinkForAppend",
-                pkField("id", DataTypes.INT().notNull()),
-                field("num", dbType("Int64"), DataTypes.BIGINT().notNull()),
-                field("ts", ClickHouseTableBuilder.dbType("DateTime64(6)"), DataTypes.TIMESTAMP()));
-    }
-
-    @Override
-    protected ClickHouseTableRow createBatchOutputTable() {
-        return clickHouseTableRow(
-                "dynamicSinkForBatch",
-                pkField("NAME", DataTypes.VARCHAR(20).notNull()),
-                field("SCORE", dbType("Int64"), DataTypes.BIGINT().notNull()));
-    }
-
-    @Override
-    protected ClickHouseTableRow createRealOutputTable() {
-        return clickHouseTableRow(
-                "REAL_TABLE", pkField("real_data", dbType("Float32"), DataTypes.FLOAT()));
-    }
-
-    @Override
-    protected List<Row> testUserData() {
-        return Arrays.asList(
-                Row.of(
-                        "user1",
-                        "Tom",
-                        "tom123@gmail.com",
-                        new BigDecimal("8.1"),
-                        new BigDecimal("16.2")),
-                Row.of(
-                        "user3",
-                        "Bailey",
-                        "bailey@qq.com",
-                        new BigDecimal("9.99"),
-                        new BigDecimal("19.98")),
-                Row.of(
-                        "user4",
-                        "Tina",
-                        "tina@gmail.com",
-                        new BigDecimal("11.3"),
-                        new BigDecimal("22.6")));
-    }
-}
+        implements ClickHouseTestBase {}
