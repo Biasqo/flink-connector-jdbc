51f625817e30acab4e7759c49891aac035bd8d64
Ares Gal <arsenag6@gmail.com>
2025-12-31 20:16:51 +0300

    renewed test 11

--- flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSourceITCase.java
+++ flink-connector-jdbc-clickhouse/src/test/java/org/apache/flink/connector/jdbc/clickhouse/table/ClickHouseDynamicTableSourceITCase.java
@@ -69,7 +69,7 @@ class ClickHouseDynamicTableSourceITCase extends JdbcDynamicTableSourceITCase
 
     @Override
     public String timestampFilterExpression() {
-        return "timestamp6_col > TO_TIMESTAMP_LTZ('2020-01-01 15:35:00', 3) "
-                + "AND timestamp6_col < TO_TIMESTAMP_LTZ('2020-01-01 15:35:01', 3)";
+        return "timestamp6_col > TO_TIMESTAMP_LTZ('2020-01-01 15:35:00') "
+                + "AND timestamp6_col < TO_TIMESTAMP_LTZ('2020-01-01 15:35:01'";
     }
 }
